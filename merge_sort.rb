a68f2f6242ade13cdc8c3724ece22389d7f775cc
guijeco <86424543+DeadlyBear11@users.noreply.github.com>
2023-01-15 20:09:00 -0700

    Fixed merge_sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,34 +1,37 @@
-def merge_sort(arr)
-  return unless arr.length > 1
-
-  if arr.length.even?
-    arr_left = arr[0..((arr.length / 2) - 1)]
-    arr_right = arr[(arr.length / 2)..]
+def merge_sort(array)
+  if array.length < 2
+    array
   else
-    arr_left = arr[0..((arr.length - 2) / 2)]
-    arr_right = arr[((arr.length - 1) / 2)..]
-  end
-
-  merge_sort(arr_left)
-  merge_sort(arr_right)
-
-  arr_sorted = []
-  idx_sorted = 0
-  idx_left = 0
-  idx_right = 0
-
-  while idx_left <= arr_left.length - 1 && idx_right <= arr_right.length - 1
-    if arr_left[idx_left] < arr_right[idx_right]
-      arr_sorted[idx_sorted] = arr_left[idx_left]
-      idx_left += 1
-    else
-      arr_sorted[idx_sorted] = arr_right[idx_right]
-      idx_right += 1
-    end
-    idx_sorted += 1
+    left = merge_sort(array[0...array.length / 2])
+    right = merge_sort(array[array.length / 2...array.length])
+    merge(left, right)
   end
 end
 
-test_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def merge(left, right, array = [])
+  (left.length + right.length).times do
+    if left.empty?
+      array.push(right.shift)
+    elsif right.empty?
+      array.push(left.shift)
+    else
+      comparison = left <=> right
+      array << case comparison
+               when -1
+                 left.shift
+               when 1
+                 right.shift
+               else
+                 left.shift
+               end
+    end
+  end
+  array
+end
 
-merge_sort(test_arr)
+arr = []
+rand(10).times do
+  arr.push(rand(200))
+end
+
+p merge_sort(arr)
