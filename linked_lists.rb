ccb574e09f44d1214f701f900b7b9cc2241cca83
guijeco <86424543+DeadlyBear11@users.noreply.github.com>
2023-01-20 11:23:22 -0700

    Created length, empty? and create_node

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,3 +1,10 @@
+# Create append method that adds a new value at the end of the List.
+# For this I need:
+# Method to create a new Node.
+# Method to check if there's Nodes in the List.
+# Method to check how many Nodes are there in the List.
+# Method to link the last Node to the new Node.
+
 class Node
   attr_reader :value
   attr_accessor :next_node
@@ -9,9 +16,27 @@ class Node
 end
 
 class LinkedList
+  attr_reader :length
+
   def initialize
     @head = 0
     @tail = nil
-    @num_nodes = 0
+    @length = 0
+  end
+
+  def create_node(value, next_node)
+    @length += 1
+    Node.new(value, next_node)
+  end
+
+  def empty?
+    length.zero?
   end
 end
+
+my_list = LinkedList.new
+p my_list.length
+p my_list.empty?
+p my_list.create_node(3000, "yeah")
+p my_list.length
+p my_list.empty?
