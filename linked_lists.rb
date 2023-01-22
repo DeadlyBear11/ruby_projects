45b7aa71c6a1c908adb00952460af9dcd642dd1e
guijeco <86424543+DeadlyBear11@users.noreply.github.com>
2023-01-22 12:04:43 -0700

    Created prepend and values

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,9 +1,7 @@
-# Create append method that adds a new value at the end of the List.
+# prepend(value) adds a new value containing value to the start of the list.
 # For this I need:
-# Method to create a new Node.
-# Method to check if there's Nodes in the List.
-# Method to check how many Nodes are there in the List.
-# Method to link the last Node to the new Node.
+# For the head to point to the new node.
+# For the new node to have a next_node attribute pointing to the last head.
 
 class Node
   attr_reader :value
@@ -22,6 +20,7 @@ class LinkedList
   def initialize
     @head = nil
     @size = 0
+    @values = []
   end
 
   def create_node(value = nil, next_node = nil)
@@ -56,13 +55,34 @@ class LinkedList
 
     false
   end
+
+  def prepend(value)
+    @head = create_node(value, head)
+  end
+
+  def values
+    @values = [] if @values
+    push_values
+  end
+
+  def push_values(node = head)
+    @values.push(node.value)
+    return @values if last_node?(node)
+
+    push_values(node.next_node)
+  end
 end
 
 my_list = LinkedList.new
-puts "The list is empty? #{my_list.empty?}. It has #{my_list.size} nodes."
+puts "Is the list empty? #{my_list.empty?}. It has #{my_list.size} nodes."
 my_list.append(3000)
-puts "The list is empty? #{my_list.empty?}. It has #{my_list.size} nodes."
+p my_list.values
+puts "Is the list empty? #{my_list.empty?}. It has #{my_list.size} nodes."
 my_list.append(6000)
 my_list.append(9000)
-puts "The list is empty? #{my_list.empty?}. It has #{my_list.size} nodes."
+p my_list.values
+puts "The list has #{my_list.size} nodes."
+my_list.prepend(2000)
+p my_list.values
+puts "The list has #{my_list.size} nodes."
 puts "The head is #{my_list.head.value}. The tail is #{my_list.tail.value}."
